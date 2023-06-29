a342cc583a16df0a21e82a60b96ade319884e0ba
Naleemp <128563950+Naleemp@users.noreply.github.com>
2023-06-29 08:09:53 -0600

    Update Jenkins-redhat8-install28.sh

--- Jenkins/Jenkins-redhat8-install28.sh
+++ Jenkins/Jenkins-redhat8-install28.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 # Author: Simon Peter Mendy
 # Installing Jenkins on RHEL 7/8, CentOS 7/8 or Amazon Linux OS
-# You can execute this script as user-data when launching your EC2 VM.
-sudo timedatectl set-timezone America/New_York
-sudo hostnamectl set-hostname jenkins
-sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade -y
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
 # Add required dependencies for the jenkins package
-sudo yum install java-11-openjdk -y
-sudo yum install jenkins -y
+sudo yum install java-11-openjdk
+sudo yum install jenkins
 sudo systemctl daemon-reload
-# start jenkins
-# Start Jenkins
-# You can enable the Jenkins service to start at boot with the command:
 sudo systemctl enable jenkins
-#You can start the Jenkins service with the command:=
 sudo systemctl start jenkins
-# You can check the status of the Jenkins service using the command:
 sudo systemctl status jenkins
 sudo su - ec2-user
 echo "echo of jenkins installation"
